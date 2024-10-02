53d4abae7cc37c58a5dd87bceb97a652704b9fe1
Danny Shen <shenfive@gmail.com>
2024-10-02 22:25:32 +0800

    Initial Commit

---
+++ HelloUIHostingController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloUIHostingController
+//
+//  Created by 申潤五 on 2024/10/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
